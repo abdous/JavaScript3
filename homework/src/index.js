7a31209ef6371b996aa5217a8fe92243b1cc4b2e
Abdou Sanda Dourhamane <abdous2004@yahoo.fr>
2018-07-25 11:42:03 +0200

    as far i have created the select element but coulnt be able to impliment the option in

--- homework/src/index.js
+++ homework/src/index.js
@@ -15,18 +15,38 @@
     xhr.onerror = () => cb(new Error('Network request failed'));
     xhr.send();
   }
-
-  function createAndAppend(name, parent, options = {}) {
-    const elem = document.createElement(name);
-    parent.appendChild(elem);
-    Object.keys(options).forEach((key) => {
-      const value = options[key];
-      if (key === 'html') {
-        elem.innerHTML = value;
-      } else {
-        elem.setAttribute(key, value);
-      }
-    });
+  // added per me
+  let header= document.createElement('header');
+    header.setAttribute("class", "header");
+    root.appendChild(header);
+      let p = document.createElement('p');
+        let text =document.createTextNode("HYF Repositories");
+        p.appendChild(text);
+        header.appendChild(p);
+      let select = document.createElement('select');
+        select.setAttribute('class', "repo-selector");
+        header.appendChild(select);
+  let container = document.createElement('div');
+      container.setAttribute('id','container');
+      root.appendChild(container);
+      let left = document.createElement('div');
+          left.setAttribute('class', 'lef-div whiteframe');
+          container.appendChild(left);
+      let right = document.createElement('div');
+          right.setAttribute('class', 'right-div whiteframe');
+          container.appendChild(right);
+// end of first add
+      function createAndAppend(name, parent, options = {}) {
+        const elem = document.createElement(name);
+        select.appendChild(elem);
+        Object.keys(options).forEach((key) => {
+          const value = options[key];
+          if (key === 'html') {
+            elem.innerHTML = value;
+          } else {
+            elem.setAttribute(key, value);
+          }
+        });
     return elem;
   }
 
@@ -36,7 +56,7 @@
       if (err) {
         createAndAppend('div', root, { html: err.message, class: 'alert-error' });
       } else {
-        createAndAppend('pre', root, { html: JSON.stringify(data, null, 2) });
+        createAndAppend('option', root, { html: JSON.stringify(data, null, 2) });
       }
     });
   }
